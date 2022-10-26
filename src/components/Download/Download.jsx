735b2450ebb4fe11894e3f6b96911fd92f6ee36d
tablonimus <lautamartin1995@gmail.com>
2022-10-26 00:46:26 -0300

    con button

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,33 +1,11 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import PWAPrompt from "react-ios-pwa-prompt";
+import { usePWAInstall } from "react-use-pwa-install";
 
 export default function Download() {
-  // const [supportsPWA, setSupportsPWA] = useState(false);
-  // const [promptInstall, setPromptInstall] = useState(null);
+  const install = usePWAInstall();
 
-  // useEffect(() => {
-  //   const handler = (e) => {
-  //     e.preventDefault();
-  //     console.log("we are being triggered :D");
-  //     setSupportsPWA(true);
-  //     setPromptInstall(e);
-  //   };
-  //   window.addEventListener("beforeinstallprompt", handler);
-
-  //   return () => window.removeEventListener("transitionend", handler);
-  // }, []);
-
-  // const onClick = (evt) => {
-  //   evt.preventDefault();
-  //   if (!promptInstall) {
-  //     return;
-  //   }
-  //   promptInstall.prompt();
-  // };
-  // if (!supportsPWA) {
-  //   return null;
-  // }
   return (
     <div className="flex flex-col justify-between gap-5  w-full items-center">
       <NavBar />
@@ -41,15 +19,14 @@ export default function Download() {
         copyBody="Agrega a Aconcagua Cup 2022 al menú principal."
         permanentlyHideOnDismiss={false}
       />
+      <div>
+        <h1>My app</h1>
+        {install && (
+          <button className="w-24 h-24 rounded-lg" onClick={install}>
+            Instalar Aplicación
+          </button>
+        )}{" "}
+      </div>
     </div>
-    // <button
-    //   className="w-96 h-96"
-    //   id="setup_button"
-    //   aria-label="Install app"
-    //   title="Install app"
-    //   onClick={onClick}
-    // >
-    //   Install
-    // </button>
   );
 }
