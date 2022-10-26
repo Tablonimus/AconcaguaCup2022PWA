fb7db3c79a5373cb0736ea2122edf3469d9fe541
tablonimus <lautamartin1995@gmail.com>
2022-10-26 00:21:07 -0300

    ahora

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar";
+import PWAPrompt from "react-ios-pwa-prompt";
 
 export default function Download() {
   // const [supportsPWA, setSupportsPWA] = useState(false);
@@ -29,7 +30,17 @@ export default function Download() {
   // }
   return (
     <div className="flex flex-col justify-between gap-5  w-full items-center">
-      <NavBar/>
+      <NavBar />
+      <PWAPrompt
+        promptOnVisit={1}
+        timesToShow={3}
+        copyShareButtonLabel="1) Presiona el botón compartir."
+        copyAddHomeButtonLabel="2) Presiona para descargar."
+        copyClosePrompt="Mas Tarde"
+        copyTitle="Descargar APP"
+        copyBody="Agrega a Aconcagua Cup 2022 al menú principal."
+        permanentlyHideOnDismiss={false}
+      />
     </div>
     // <button
     //   className="w-96 h-96"
